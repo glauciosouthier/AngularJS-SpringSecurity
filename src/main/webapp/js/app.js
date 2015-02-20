90e081e8149d7a1d110997bd1216ad2f71202ad2
Admin <Admin@LG1>
2015-02-20 11:02:03 -0200

    changes

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,137 +1,167 @@
 
 var logEnabled =true;
-angular.module('exampleApp', ['ngRoute', 'ngCookies', 'exampleApp.services'])
+$(document).bind("mobileinit", function(){
+    $.mobile.page.prototype.options.domCache = true ;
+    $.mobile.allowCrossDomainPages = true;
+});
+var services = angular.module('exampleApp.services', ['ngResource']);
+
+var mapModule = angular.module('MapModule', ['ngMap']);
+
+mapModule.service('MapService', function() { this.show= function ($scope) {showMap($scope)};});
+
+var exampleApp = angular.module('exampleApp', ['ngRoute', 'ngCookies', 'exampleApp.services','MapModule'])
 	.config(
 		[ '$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
 			
-			$routeProvider.when('/create', {
-				templateUrl: 'partials/create.html',
-				controller: CreateController
-			});
+			configureRoutes($routeProvider,$locationProvider);
+			configureInterceptors($httpProvider);
 			
-			$routeProvider.when('/edit/:id', {
-				templateUrl: 'partials/edit.html',
-				controller: EditController
-			});
-			
-			$routeProvider.when('/delete/:id', {
-				templateUrl: 'partials/index.html',
-				controller: IndexController
-			});
-
-			$routeProvider.when('/login', {
-				templateUrl: 'partials/login.html',
-				controller: LoginController
-			});
-			
-			$routeProvider.otherwise({
-				templateUrl: 'partials/index.html',
-				controller: IndexController
-			});
-			
-			$locationProvider.hashPrefix('!');
-			
-			/*
-			 * Register error provider that shows message on failed requests or
-			 * redirects to login page on unauthenticated requests
-			 */
-		    $httpProvider.interceptors.push(function ($q, $rootScope, $location) {
-			        return {
-			        	'responseError': function(rejection) {
-			        		var status = rejection.status;
-			        		var config = rejection.config;
-			        		var method = config.method;
-			        		var url = config.url;
-			      
-			        		if (status == 401) {
-			        			$location.path( "/login" );
-			        		} else {
-			        			$rootScope.error = method + " on " + url + " failed with status " + status;
-			        		}
-			              
-			        		return $q.reject(rejection);
-			        	}
-			        };
-			    }
-		    );
-		    
-		    /*
-			 * Registers auth token interceptor, auth token is either passed by
-			 * header or by query parameter as soon as there is an authenticated
-			 * user
-			 */
-		    $httpProvider.interceptors.push(function ($q, $rootScope, $location) {
-		        return {
-		        	'request': function(config) {
-		        		var isRestCall = config.url.contains('rest');
-		        		if (isRestCall && angular.isDefined($rootScope.authToken)) {
-		        			var authToken = $rootScope.authToken;
-		        			if (exampleAppConfig.useAuthTokenHeader) {
-		        				config.headers['X-Auth-Token'] = authToken;
-		        			} else {
-		        				config.url = config.url + "?token=" + authToken;
-		        			}
-		        			
-		        		}
-		        		return config || $q.when(config);
-		        	}
-		        };
-		    }
-	    );
-		   
 		} ]
 		
 	).run(function($rootScope, $location, $cookieStore, UserService,DataFactory) {
+		 bootstrap($rootScope, $location, $cookieStore, UserService,DataFactory);
+	});
+
+
+
+function bootstrap($rootScope, $location, $cookieStore, UserService,DataFactory){
+	
+	/* Reset error when a new view is loaded */
+	$rootScope.$on('$viewContentLoaded', function() {
+		delete $rootScope.error;
+	});
+	
+	$rootScope.hasRole = function(role) {
 		
-		/* Reset error when a new view is loaded */
-		$rootScope.$on('$viewContentLoaded', function() {
-			delete $rootScope.error;
-		});
-		
-		$rootScope.hasRole = function(role) {
-			
-			if ($rootScope.user === undefined) {
-				return false;
-			}
-			
-			if ($rootScope.user.roles[role] === undefined) {
-				return false;
-			}
-			
-			return $rootScope.user.roles[role];
-		};
-		
-		$rootScope.logout = function() {
-			delete $rootScope.user;
-			delete $rootScope.authToken;
-			$cookieStore.remove('authToken');
-			$location.path("/login");
-		};
-		
-		 /* Try getting valid user from cookie or go to login page */
-		var originalPath = $location.path();
-		$location.path("/login");
-		var authToken = $cookieStore.get('authToken');
-		if (authToken !== undefined) {
-			$rootScope.authToken = authToken;
-			//**
-			/*
-			UserService.get(function(user) {
-				$rootScope.user = user;
-				$location.path(originalPath);
-			});
-			*/
-			DataFactory.getUser().success(function (user) {
-				$rootScope.user = user;
-				$location.path(originalPath);
-			}) .error(function (error) {
-		    	console.log('Unable to load user data: ' + error.message);
-		        
-		    });
+		if ($rootScope.user === undefined) {
+			return false;
 		}
 		
-		$rootScope.initialized = true;
+		if ($rootScope.user.roles[role] === undefined) {
+			return false;
+		}
+		
+		return $rootScope.user.roles[role];
+	};
+	
+	$rootScope.logout = function() {
+		delete $rootScope.user;
+		delete $rootScope.authToken;
+		$cookieStore.remove('authToken');
+		$location.path("/login");
+	};
+	
+	 /* Try getting valid user from cookie or go to login page */
+	var originalPath = $location.path();
+	$location.path("/login");
+	var authToken = $cookieStore.get('authToken');
+	if (authToken !== undefined) {
+		$rootScope.authToken = authToken;
+		//**
+		/*
+		UserService.get(function(user) {
+			$rootScope.user = user;
+			$location.path(originalPath);
+		});
+		*/
+		DataFactory.getUser().success(function (user) {
+			$rootScope.user = user;
+			$location.path(originalPath);
+		}) .error(function (error) {
+	    	console.log('Unable to load user data: ' + error.message);
+	        
+	    });
+	}
+	
+	$rootScope.initialized = true;
+}
+
+
+function configureRoutes($routeProvider,$locationProvider){
+	$routeProvider.when('/create', {
+		templateUrl: 'partials/create.html',
+		controller: CreateController
 	});
+	
+	$routeProvider.when('/edit/:id', {
+		templateUrl: 'partials/edit.html',
+		controller: EditController
+	});
+	
+	$routeProvider.when('/delete/:id', {
+		templateUrl: 'partials/index.html',
+		controller: IndexController
+	});
+
+	$routeProvider.when('/login', {
+		templateUrl: 'partials/login.html',
+		controller: LoginController
+	});
+	
+	$routeProvider.when('/map', {
+		templateUrl: 'partials/map.html',
+		controller: MapController
+	});
+	
+	$routeProvider.otherwise({
+		templateUrl: 'partials/index.html',
+		controller: IndexController
+	});
+	
+	$locationProvider.hashPrefix('!');
+}
+
+
+function configureInterceptors($httpProvider){
+	/*
+	 * Register error provider that shows message on failed requests or
+	 * redirects to login page on unauthenticated requests
+	 */
+    $httpProvider.interceptors.push(function ($q, $rootScope, $location) {
+	        return {
+	        	'responseError': function(rejection) {
+	        		var status = rejection.status;
+	        		var config = rejection.config;
+	        		var method = config.method;
+	        		var url = config.url;
+	      
+	        		if (status == 401) {
+	        			$location.path( "/login" );
+	        		} else {
+	        			$rootScope.error = method + " on " + url + " failed with status " + status;
+	        		}
+	              
+	        		return $q.reject(rejection);
+	        	}
+	        };
+	    }
+    );
+    
+    /*
+	 * Registers auth token interceptor, auth token is either passed by
+	 * header or by query parameter as soon as there is an authenticated
+	 * user
+	 */
+    $httpProvider.interceptors.push(function ($q, $rootScope, $location) {
+        return {
+        	'request': function(config) {
+        		var isRestCall = config.url.contains('rest');
+        		if (isRestCall && angular.isDefined($rootScope.authToken)) {
+        			var authToken = $rootScope.authToken;
+        			if (exampleAppConfig.useAuthTokenHeader) {
+        				config.headers['X-Auth-Token'] = authToken;
+        			} else {
+        				config.url = config.url + "?token=" + authToken;
+        			}
+        			
+        		}
+        		return config || $q.when(config);
+        	}
+        };
+    }
+);
+}
 
 
 function IndexController($scope, NewsService,DataFactory) {
@@ -278,3 +308,13 @@ function LoginController($scope, $rootScope, $location, $cookieStore, UserServic
 };
 
 
+function MapController($scope, $location, DataFactory,MapService) {
+	/*MapService.show($scope)
+	.success(function (result) {
+		//$location.path('/');
+	})
+	.error(function (error) {
+    	console.log('Unable to load map data: ' + error.message);
+        
+    });*/
+};
